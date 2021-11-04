c59f17dc3ccd8b8f86b7d4f26f4447432c670196
ZhengYu, Xu <zen-xu@outlook.com>
2021-11-04 23:58:38 +0800

    refactor: use config2 replace config in main.rs

--- src/bin/pd/main.rs
+++ src/bin/pd/main.rs
@@ -3,10 +3,12 @@ use std::fmt::Debug;
 use std::path::PathBuf;
 
 use clap::{crate_authors, crate_description};
-use dialoguer::Confirm;
 use dialoguer::{theme::ColorfulTheme, Input, Select};
+use dialoguer::{Confirm, MultiSelect as DialoguerMultiSelect};
 use miette::Result;
-use petridish::config::{PromptConfig, Value};
+use petridish::config2::{
+    MultiSelect, MultiSelectType, PromptConfig, SingleSelect, SingleSelectType, Value,
+};
 use petridish::render::Render;
 use petridish::source::new_source;
 use structopt::clap::AppSettings::{ColorAuto, ColoredHelp};
@@ -59,87 +61,7 @@ fn main() -> Result<()> {
 
     for prompt in config.prompts {
         match prompt.kind {
-            petridish::config::PromptKind::SingleChoice {
-                default,
-                choices,
-                multi: _,
-            } => {
-                let default: usize = match default {
-                    Some(default) => *(&choices.iter().position(|i| i == &default).unwrap()),
-                    None => 0,
-                };
-                let selection = Select::with_theme(&ColorfulTheme::default())
-                    .with_prompt(prompt.message.unwrap_or(prompt.name.clone()))
-                    .default(default)
-                    .items(&choices)
-                    .interact()
-                    .unwrap();
-                let value = &choices[selection];
-                match value {
-                    petridish::config::Value::Number(v) => {
-                        context.insert(prompt.name, &v.as_f64().unwrap());
-                    }
-                    petridish::config::Value::String(s) => {
-                        context.insert(prompt.name, s);
-                    }
-                }
-            }
-            petridish::config::PromptKind::MultiChoices {
-                default: _,
-                choices: _,
-                multi: _,
-            } => {
-                // let defaults = {
-                //     match default {
-                //         Some(defaults) => choices
-                //             .iter()
-                //             .map(|choice| defaults.contains(choice))
-                //             .collect(),
-                //         None => vec![false; choices.len()],
-                //     }
-                // };
-
-                // let prompt_message = prompt.message.unwrap_or(prompt.name.clone());
-
-                // let selections = loop {
-                //     let selections = MultiSelect::with_theme(&ColorfulTheme::default())
-                //         .with_prompt(&prompt_message)
-                //         .items(&choices[..])
-                //         .defaults(&defaults[..])
-                //         .interact()
-                //         .unwrap();
-                //     if selections.is_empty() {
-                //         println!("You did not select anything :(");
-                //         continue;
-                //     }
-                //     break selections;
-                // };
-                // let selections: Vec<&Value> = selections.iter().map(|idx| &choices[*idx]).collect();
-                // let selections = selections.iter().map(|v| {
-                //     match v {
-                //         Value::Number(v) => {
-                //             v.as_f64().unwrap()
-                //         },
-                //         Value::String(s) => {
-                //             s
-                //         },
-                //     }
-                // })
-            }
-            petridish::config::PromptKind::Confirm {
-                confirm: _,
-                default,
-            } => {
-                let confirm = Confirm::with_theme(&ColorfulTheme::default())
-                    .with_prompt(prompt.message.unwrap_or(prompt.name.clone()))
-                    .default(default)
-                    .show_default(true)
-                    .wait_for_newline(true)
-                    .interact()
-                    .unwrap();
-                context.insert(prompt.name, &confirm);
-            }
-            petridish::config::PromptKind::Normal { default } => match default {
+            petridish::config2::PromptKind::Default { default } => match default {
                 Some(Value::String(default)) => {
                     let default = tera.render_str(&default, &context).unwrap();
                     let value = Input::with_theme(&ColorfulTheme::default())
@@ -164,6 +86,133 @@ fn main() -> Result<()> {
                         .interact_text()
                         .unwrap();
                     context.insert(prompt.name, &value);
+                }
+            },
+            petridish::config2::PromptKind::Confirm {
+                confirm: _,
+                default,
+            } => {
+                let confirm = Confirm::with_theme(&ColorfulTheme::default())
+                    .with_prompt(prompt.message.unwrap_or(prompt.name.clone()))
+                    .default(default)
+                    .show_default(true)
+                    .wait_for_newline(true)
+                    .interact()
+                    .unwrap();
+                context.insert(prompt.name, &confirm);
+            }
+            petridish::config2::PromptKind::SingleSelect(value_type) => match value_type {
+                SingleSelectType::String(SingleSelect {
+                    default,
+                    choices,
+                    multi: _,
+                }) => {
+                    let default: usize = match default {
+                        Some(default) => *(&choices.iter().position(|i| i == &default).unwrap()),
+                        None => 0,
+                    };
+                    let selection = Select::with_theme(&ColorfulTheme::default())
+                        .with_prompt(prompt.message.unwrap_or(prompt.name.clone()))
+                        .default(default)
+                        .items(&choices)
+                        .interact()
+                        .unwrap();
+                    let value = &choices[selection];
+                    context.insert(prompt.name, value);
+                }
+                SingleSelectType::Number(SingleSelect {
+                    default,
+                    choices,
+                    multi: _,
+                }) => {
+                    let default: usize = match default {
+                        Some(default) => *(&choices.iter().position(|i| i == &default).unwrap()),
+                        None => 0,
+                    };
+                    let selection = Select::with_theme(&ColorfulTheme::default())
+                        .with_prompt(prompt.message.unwrap_or(prompt.name.clone()))
+                        .default(default)
+                        .items(&choices)
+                        .interact()
+                        .unwrap();
+                    let value = &choices[selection];
+                    context.insert(prompt.name, &value.as_f64().unwrap());
+                }
+            },
+            petridish::config2::PromptKind::MultiSelect(value_type) => match value_type {
+                MultiSelectType::String(MultiSelect {
+                    default,
+                    choices,
+                    multi: _,
+                }) => {
+                    let defaults = {
+                        match default {
+                            Some(defaults) => choices
+                                .iter()
+                                .map(|choice| defaults.contains(choice))
+                                .collect(),
+                            None => vec![false; choices.len()],
+                        }
+                    };
+
+                    let prompt_message = prompt.message.unwrap_or(prompt.name.clone());
+
+                    let selections = loop {
+                        let selections =
+                            DialoguerMultiSelect::with_theme(&ColorfulTheme::default())
+                                .with_prompt(&prompt_message)
+                                .items(&choices[..])
+                                .defaults(&defaults[..])
+                                .interact()
+                                .unwrap();
+                        if selections.is_empty() {
+                            println!("You did not select anything :(");
+                            continue;
+                        }
+                        break selections;
+                    };
+                    let selections = selections
+                        .iter()
+                        .map(|idx| choices[*idx].clone())
+                        .collect::<Vec<_>>();
+                    context.insert(prompt.name, &selections);
+                }
+                MultiSelectType::Number(MultiSelect {
+                    default,
+                    choices,
+                    multi: _,
+                }) => {
+                    let defaults = {
+                        match default {
+                            Some(defaults) => choices
+                                .iter()
+                                .map(|choice| defaults.contains(choice))
+                                .collect(),
+                            None => vec![false; choices.len()],
+                        }
+                    };
+
+                    let prompt_message = prompt.message.unwrap_or(prompt.name.clone());
+
+                    let selections = loop {
+                        let selections =
+                            DialoguerMultiSelect::with_theme(&ColorfulTheme::default())
+                                .with_prompt(&prompt_message)
+                                .items(&choices[..])
+                                .defaults(&defaults[..])
+                                .interact()
+                                .unwrap();
+                        if selections.is_empty() {
+                            println!("You did not select anything :(");
+                            continue;
+                        }
+                        break selections;
+                    };
+                    let selections = selections
+                        .iter()
+                        .map(|idx| choices[*idx].as_f64().unwrap())
+                        .collect::<Vec<_>>();
+                    context.insert(prompt.name, &selections);
                 }
             },
         }
