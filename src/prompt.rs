2ff6c5f69376208406142996a7a02bdd2e47ea7c
ZhengYu, Xu <zen-xu@outlook.com>
2022-06-21 22:12:08 +0800

    Impl `SingleSelector` and `MultiSelector`

--- src/prompt.rs
+++ src/prompt.rs
@@ -46,6 +46,43 @@ macro_rules! literal_bool {
 literal_bool!(true, LiteralTrue);
 literal_bool!(false, LiteralFalse);
 
+#[derive(Deserialize, Debug, PartialEq)]
+#[serde(untagged)]
+pub enum SingleSelector {
+    String(SingleSelectorConfig<String>),
+    Number(SingleSelectorConfig<f64>),
+}
+
+#[derive(Deserialize, Debug, PartialEq)]
+pub struct SingleSelectorConfig<T> {
+    pub default: Option<T>,
+    pub choices: Vec<T>,
+    pub multi: Option<LiteralFalse>,
+}
+
+#[derive(Deserialize, Debug, PartialEq)]
+#[serde(untagged)]
+pub enum MultiSelector {
+    String(MultiSelectorConfig<String>),
+    Number(MultiSelectorConfig<f64>),
+}
+
+#[derive(Deserialize, Debug, PartialEq)]
+pub struct MultiSelectorConfig<T> {
+    pub default: Option<Vec<T>>,
+    pub choices: Vec<T>,
+    pub multi: Option<LiteralTrue>,
+    #[serde(default)]
+    pub emptyable: bool,
+}
+
+#[derive(Deserialize, Debug, PartialEq)]
+#[serde(untagged)]
+pub enum PromptKind {
+    SingleSelector(SingleSelector),
+    MultiSelector(MultiSelector),
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -69,5 +106,232 @@ mod tests {
             LiteralFalse
         );
         assert!(serde_yaml::from_str::<LiteralFalse>("true").is_err())
+    }
+
+    #[test]
+    fn test_single_selector() {
+        let config = "\
+---
+default: 1
+choices: [1, 2, 3]
+";
+        let parsed = serde_yaml::from_str::<SingleSelector>(config).unwrap();
+        assert_eq!(
+            parsed,
+            SingleSelector::Number(SingleSelectorConfig {
+                default: Some(1_f64),
+                choices: vec![1_f64, 2_f64, 3_f64],
+                multi: None,
+            })
+        )
+    }
+
+    #[test]
+    fn test_single_selector_with_multi_field() {
+        let config = "\
+---
+default: 1
+choices: [1, 2, 3]
+multi: false
+";
+        let parsed = serde_yaml::from_str::<SingleSelector>(config).unwrap();
+        assert_eq!(
+            parsed,
+            SingleSelector::Number(SingleSelectorConfig {
+                default: Some(1_f64),
+                choices: vec![1_f64, 2_f64, 3_f64],
+                multi: Some(LiteralFalse),
+            })
+        )
+    }
+
+    #[test]
+    fn test_single_selector_for_string() {
+        let config = "\
+---
+default: a
+choices: [a, b, c]
+";
+        let parsed = serde_yaml::from_str::<SingleSelector>(config).unwrap();
+        assert_eq!(
+            parsed,
+            SingleSelector::String(SingleSelectorConfig {
+                default: Some("a".into()),
+                choices: vec!["a".into(), "b".into(), "c".into()],
+                multi: None,
+            })
+        )
+    }
+
+    #[test]
+    fn test_single_selector_with_different_types_between_default_and_choices() {
+        let config = "\
+---
+default: 1
+choices: [a, b, c]
+";
+        assert!(serde_yaml::from_str::<SingleSelector>(config).is_err());
+    }
+
+    #[test]
+    fn test_single_selector_with_mixed_types() {
+        let config = "\
+---
+default: a
+choices: [a, 1, c]
+";
+        assert!(serde_yaml::from_str::<SingleSelector>(config).is_err());
+    }
+
+    #[test]
+    fn test_multi_selector() {
+        let config = "\
+---
+default: [1]
+choices: [1, 2, 3]
+";
+        let parsed = serde_yaml::from_str::<MultiSelector>(config).unwrap();
+        assert_eq!(
+            parsed,
+            MultiSelector::Number(MultiSelectorConfig {
+                default: Some(vec![1_f64]),
+                choices: vec![1_f64, 2_f64, 3_f64],
+                multi: None,
+                emptyable: false,
+            })
+        )
+    }
+
+    #[test]
+    fn test_multi_selector_with_multi_field() {
+        let config = "\
+---
+default: [1]
+choices: [1, 2, 3]
+multi: true
+";
+        let parsed = serde_yaml::from_str::<MultiSelector>(config).unwrap();
+        assert_eq!(
+            parsed,
+            MultiSelector::Number(MultiSelectorConfig {
+                default: Some(vec![1_f64]),
+                choices: vec![1_f64, 2_f64, 3_f64],
+                multi: Some(LiteralTrue),
+                emptyable: false,
+            })
+        )
+    }
+
+    #[test]
+    fn test_multi_selector_for_string() {
+        let config = "\
+---
+default: [a]
+choices: [a, b, c]
+";
+        let parsed = serde_yaml::from_str::<MultiSelector>(config).unwrap();
+        assert_eq!(
+            parsed,
+            MultiSelector::String(MultiSelectorConfig {
+                default: Some(vec!["a".into()]),
+                choices: vec!["a".into(), "b".into(), "c".into()],
+                multi: None,
+                emptyable: false,
+            })
+        )
+    }
+
+    #[test]
+    fn test_multi_selector_with_different_types_between_default_and_choices() {
+        let config = "\
+---
+default: [1]
+choices: [a, b, c]
+";
+        assert!(serde_yaml::from_str::<MultiSelector>(config).is_err());
+    }
+
+    #[test]
+    fn test_multi_selector_with_mixed_types() {
+        let config = "\
+---
+default: [a]
+choices: [a, 1, c]
+";
+        assert!(serde_yaml::from_str::<MultiSelector>(config).is_err());
+    }
+
+    #[test]
+    fn test_prompt_kind_for_single_selector() {
+        let config = "\
+---
+default: 1
+choices: [1, 2, 3]
+";
+        let parsed = serde_yaml::from_str::<PromptKind>(config).unwrap();
+        assert_eq!(
+            parsed,
+            PromptKind::SingleSelector(SingleSelector::Number(SingleSelectorConfig {
+                default: Some(1_f64),
+                choices: vec![1_f64, 2_f64, 3_f64],
+                multi: None,
+            }))
+        )
+    }
+
+    #[test]
+    fn test_prompt_kind_for_multi_selector() {
+        let config = "\
+---
+default: [1]
+choices: [1, 2, 3]
+";
+        let parsed = serde_yaml::from_str::<PromptKind>(config).unwrap();
+        assert_eq!(
+            parsed,
+            PromptKind::MultiSelector(MultiSelector::Number(MultiSelectorConfig {
+                default: Some(vec![1_f64]),
+                choices: vec![1_f64, 2_f64, 3_f64],
+                multi: None,
+                emptyable: false
+            }))
+        )
+    }
+
+    #[test]
+    fn test_prompt_kind_only_with_choices() {
+        let config = "\
+---
+choices: [1, 2, 3]
+";
+        let parsed = serde_yaml::from_str::<PromptKind>(config).unwrap();
+        assert_eq!(
+            parsed,
+            PromptKind::SingleSelector(SingleSelector::Number(SingleSelectorConfig {
+                default: None,
+                choices: vec![1_f64, 2_f64, 3_f64],
+                multi: None,
+            }))
+        )
+    }
+
+    #[test]
+    fn test_prompt_kind_with_choices_and_multi_on() {
+        let config = "\
+---
+choices: [1, 2, 3]
+multi: true
+";
+        let parsed = serde_yaml::from_str::<PromptKind>(config).unwrap();
+
+        assert_eq!(
+            parsed,
+            PromptKind::MultiSelector(MultiSelector::Number(MultiSelectorConfig {
+                default: None,
+                choices: vec![1_f64, 2_f64, 3_f64],
+                multi: Some(LiteralTrue),
+                emptyable: false
+            }))
+        )
     }
 }
