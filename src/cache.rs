8f3f15d7a76a3429ce2d8caf092cf0ad57e4db40
ZhengYu, Xu <zen-xu@outlook.com>
2022-07-04 13:15:57 +0800

    Add new mod `cache`

---
+++ src/cache.rs
@@ -0,0 +1,38 @@
+use dirs::cache_dir;
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
+
+pub struct Cache;
+
+/// Returns the path to the user's repository cache directory.
+///
+///
+/// |Platform | Example                                                         |
+/// | ------- | --------------------------------------------------------------- |
+/// | Linux   | /home/alice/.config/petridish/repositories                      |
+/// | macOS   | /Users/Alice/Library/Application Support/petridish/repositories |
+/// | Windows | C:\Users\Alice\AppData\Roaming\petridish\repositories           |
+fn get_cache_dir() -> PathBuf {
+    cache_dir().unwrap().join("petridish/repositories")
+}
+
+impl Cache {
+    pub fn get(name: &str) -> Option<PathBuf> {
+        let path = get_cache_dir().join(name);
+        if path.exists() {
+            Some(path)
+        } else {
+            None
+        }
+    }
+
+    pub fn add(source: &Path) {
+        let dest = get_cache_dir().join(source.file_name().unwrap().to_str().unwrap());
+        if dest.exists() {
+            fs::remove_dir_all(&dest).unwrap();
+        }
+        fs::rename(source, dest).unwrap();
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod cache;
 pub mod config;
 pub mod error;
 mod literal_value;
